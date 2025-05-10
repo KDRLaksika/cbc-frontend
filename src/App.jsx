4765e3928cce102766ac968c2b4ac19fd3b78d64
KDRLaksika <135639511+KDRLaksika@users.noreply.github.com>
2025-05-10 09:30:19 +0530

    Create mediaUpload.jsx and textPage2.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AdminPage from './pages/adminPage'
 import TestPage from './pages/testPage'
 import { Toaster } from 'react-hot-toast'
 import RegisterPage from './pages/register'
+import TestPage2 from './pages/testPage2'
 
 function App() {
   
@@ -22,6 +23,7 @@ function App() {
       <Route path="/signup" element={<RegisterPage />} />
       <Route path="/admin/*" element={<AdminPage />} />
       <Route path="/testing" element={<TestPage />} />
+      <Route path="/testing2" element={<TestPage2 />} />
       <Route path="/*" element={<h1>Page Not Found 404 Error</h1>} />
     </Routes>
    </div>
@@ -30,3 +32,7 @@ function App() {
 }
 
 export default App
+
+
+//https://nsqamuhdendvtakberya.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcWFtdWhkZW5kdnRha2JlcnlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzY0NzAsImV4cCI6MjA2MjQxMjQ3MH0.W0ondifODnrQDTAT4A8uLWBdGNKzOeN53jvLuS_va6M
