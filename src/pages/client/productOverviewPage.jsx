78d9ac6b42810ea779b61df8a2719ad5cfad12df
KDRLaksika <135639511+KDRLaksika@users.noreply.github.com>
2025-06-24 19:42:00 +0530

    Create and update productOverviewPage

--- src/pages/client/productOverviewPage.jsx
+++ src/pages/client/productOverviewPage.jsx
@@ -3,6 +3,8 @@ import { use, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
+import ImageSlider from '../../components/imageSlider';
+import Loading from '../../components/loading';
 
 export default function ProductOverviewPage() {
 
@@ -27,10 +29,53 @@ export default function ProductOverviewPage() {
   }, []);
 
   return (
-    
-    <div className='bg-accent font-fancy'>
-      <p>This is the product overview page for product ID: {JSON.stringify(product)}</p>
-    </div>
+    <>
+      {status == "success" && (
+        <div className="w-full h-full flex">
+          <div className="w-[50%] h-full flex justify-center items-center">
+            <ImageSlider images={product.images} />
+          </div>
+
+          <div className="w-[50%] flex justify-center items-center h-full">
+            <div className="w-[500px] h-[600px] flex flex-col items-center">
+              <h1 className="w-full text-center text-4xl text-secondary font-semibold">{product.name}
+                  {
+                      Array.isArray(product.altName) && product.altName.map((altName, index) => (
+                        <span key={index} className="text-4xl text-gray-600">{" | " + altName}</span>
+                      ))
+                  }
+              </h1>
+              <h1 className="w-full text-center my-2 text-md text-gray-600 font-semibold">{product.productId}</h1>
+              <h1 className="w-full text-center my-2 text-2xl text-gray-800 font-semibold">{product.description}</h1>
+              {
+                product.labelledPrice > product.price ?
+                <div>
+                  <span className='text-4xl mx-4 text-gray-500 line-through'>{product.labelledPrice.toFixed(2)} </span>
+                  <span className='text-4xl mx-4 text-accent font-bold'>${product.price.toFixed(2)}</span>
+                </div>
+                :
+                <div>
+                  <span className='text-4xl mx-4 text-accent font-bold'>${product.price.toFixed(2)}</span>
+                </div>
+              }
+
+              <div className="w-full flex justify-center items-center mt-4">
+                <button className="w-[200px] h-[50px] bg-accent mx-4 cursor-pointer text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">
+                  Add to Cart
+                </button>
+                <button className="w-[200px] h-[50px] bg-accent mx-4 cursor-pointer text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">
+                  Buy Now
+                </button>
+            </div>
+            
+              
+            </div>
+          </div>
+        </div>
+      )}
+
+      {status == "loading" && <Loading />}
+    </>
   );
 }
 
